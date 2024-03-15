eccb5b8df02d4fa7172a87b0b95b098b7746253e
JuanDavidSerna <jserna@cloudlabs.co>
2024-03-15 13:32:54 -0500

    PracticManipDataJdbc

--- src/main/java/org/juandavid/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/juandavid/java/jdbc/EjemploJdbc.java
@@ -1,30 +1,25 @@
 package org.juandavid.java.jdbc;
 
-import java.sql.*;
+import org.juandavid.java.jdbc.modelo.Producto;
+import org.juandavid.java.jdbc.repositorio.ProductoRepositorioImpl;
+import org.juandavid.java.jdbc.repositorio.Repositorio;
+import org.juandavid.java.jdbc.util.ConexionBaseDatos;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_practica?serverTimeZone=UTC";
-        String username = "root";
-        String password = "12345";
+        try (Connection conn = ConexionBaseDatos.getInstance()) {
 
-        try (Connection conn = DriverManager.getConnection(url,username,password);
-             Statement stmt = conn.createStatement();
-             ResultSet result = stmt.executeQuery("SELECT * FROM  productos");)
-            {
-            while (result.next()){
-                System.out.print(result.getInt("id"));
-                System.out.print("| ");
-                System.out.print(result.getString("nombre"));
-                System.out.print("| ");
-                System.out.print(result.getInt("precio"));
-                System.out.print("| ");
-                System.out.println(result.getDate("fecha_registro"));
-            }
+            Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            repositorio.listar().forEach(p-> System.out.println(p.getNombre()));
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
 
-        }
         }
     }
+}
+
