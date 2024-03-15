da7b07810caa027f28e7ce54e64506a3f1f0b0b9
JuanDavidSerna <jserna@cloudlabs.co>
2024-03-15 12:18:35 -0500

    PracticManipDataJdbc

--- src/main/java/org/juandavid/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/juandavid/java/jdbc/EjemploJdbc.java
@@ -5,25 +5,26 @@ import java.sql.*;
 public class EjemploJdbc {
     public static void main(String[] args) {
 
-
         String url = "jdbc:mysql://localhost:3306/java_practica?serverTimeZone=UTC";
         String username = "root";
         String password = "12345";
-        try {
-            Connection conn = DriverManager.getConnection(url,username,password);
-            Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery("SELECT * FROM  productos");
 
+        try (Connection conn = DriverManager.getConnection(url,username,password);
+             Statement stmt = conn.createStatement();
+             ResultSet result = stmt.executeQuery("SELECT * FROM  productos");)
+            {
             while (result.next()){
-                System.out.println(result.getString("nombre"));
+                System.out.print(result.getInt("id"));
+                System.out.print("| ");
+                System.out.print(result.getString("nombre"));
+                System.out.print("| ");
+                System.out.print(result.getInt("precio"));
+                System.out.print("| ");
+                System.out.println(result.getDate("fecha_registro"));
             }
-            result.close();
-            stmt.close();
-            conn.close();
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
 
+        }
+        }
     }
-}
